b63e54d7219d9de8918d3f123c374f897be309d3
Cyril Quinton <cyril.quinton@free.fr>
2017-08-25 13:44:37 +0200

    changed webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'bundle.js',
+    filename: 'formx.js',
     library: 'formx',
 
     path: path.resolve(__dirname, 'dist')
